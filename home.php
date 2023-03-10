e8a3cb4fae11641b58e386c7b1cb949440ffe892
starksinclair <83012040+starksinclair@users.noreply.github.com>
2023-03-10 20:39:05 +0000

    Add files via upload

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sideHustle weekly Task 1 | PHP</title>
+</head>
+<body>
+    <?php 
+        $myName = "Nzenwata Sinclair";
+        echo $myName;
+    ?>    
+</body>
+</html>
